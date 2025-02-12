4b12c3c41bfca0b0b062580897d8b401125b92ef
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-02-12 18:10:47 +0530

    Add brands data

---
+++ data/brands.ts
@@ -0,0 +1,52 @@
+export const brands = [
+    {
+        id: 1,
+        name: "7Span",
+        logo: "/images/brands/7span.png",
+    },
+    {
+        id: 2,
+        name: "Codelessly",
+        logo: "/images/brands/codelessly.png",
+    },
+    {
+        id: 3,
+        name: "Flutter",
+        logo: "/images/brands/flutter.png",
+    },
+    {
+        id: 4,
+        name: "HDFC Sky",
+        logo: "/images/brands/hdfc_sky.png",
+    },
+    {
+        id: 5,
+        name: "NonStop",
+        logo: "/images/brands/nonstop.png",
+    },
+    {
+        id: 6,
+        name: "Serverpod",
+        logo: "/images/brands/serverpod.png",
+    },
+    {
+        id: 7,
+        name: "Tata Digital Private Limited",
+        logo: "/images/brands/tata_digital.png",
+    },
+    {
+        id: 8,
+        name: "Tide",
+        logo: "/images/brands/tide.png",
+    },
+    {
+        id: 9,
+        name: "Welltested.ai",
+        logo: "/images/brands/welltested_ai.png",
+    },
+    {
+        id: 10,
+        name: "Zoho",
+        logo: "/images/brands/zoho.png",
+    },
+]; 
